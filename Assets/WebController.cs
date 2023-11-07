9ee1fc6dc7c2020f27df1ed596fd5bf0baad25d7
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-11-07 12:39:50 -0500

    Change start to awake

--- Assets/WebController.cs
+++ Assets/WebController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class WebController : MonoBehaviour
 {
+    public SpringJoint web;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         
     }
