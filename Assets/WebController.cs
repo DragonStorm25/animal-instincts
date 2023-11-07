c34d078cef8fca4de1d3ccb7d8fe00d22ec21f34
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2023-11-07 12:42:59 -0500

    Add basic code from template game

--- Assets/WebController.cs
+++ Assets/WebController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class WebController : MonoBehaviour
 {
     public SpringJoint web;
+    private bool webToggle = false;
 
     // Start is called before the first frame update
     void Awake()
@@ -15,6 +16,23 @@ public class WebController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+            {
+                webToggle = !webToggle;
+                Vector3 mousePos = Input.mousePosition;
+                mousePos.z = 0;
+                Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+                Debug.Log(worldPosition);
+
+                if (webToggle)
+                {
+                    web.anchor = worldPosition;
+                    web.spring = 10;
+                }
+                else
+                {
+                    web.spring = 0;
+                }
+            }
     }
 }
